db7893c30e6058631f4e06e3aa9693931795b383
Christopher Bogan <professorx71@icloud.com>
2020-10-07 18:56:04 -0400

    propType validation

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { format } from 'date-fns'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Note.css'
@@ -28,4 +28,7 @@ export default function Note(props) {
       </div>
     </div>
   )
+}
+Note.propType={
+  Note__title: []
 }
